5586b1d1bd851a45e296a23483acb916d8fe334d
savayer <sava0094@gmail.com>
2018-10-21 15:14:50 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chat with Pusher API
+
+[View demo](https://chat.savayer.space)
+
+### Installation
+
+create .env file with your app keys, then run commands:
+
+```sh
+$ composer install
+```
+
+and if you need 
+
+```sh
+$ npm i
+```
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+	cleanCSS = require('gulp-clean-css'),
+	sass = require('gulp-sass'),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'dist',
+		livereload: true
+	})
+})
+
+gulp.task('convertCss', function() {
+    gulp.src('scss/styles.scss')
+		.pipe(sass())
+		.pipe(gulp.dest("css"))
+		.pipe(connect.reload())
+	gulp.src('css/styles.css')
+    	.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('css/min'))
+		.pipe(connect.reload())
+});
+
+gulp.task('html', function() {
+	gulp.src('index.php')
+		.pipe(connect.reload())
+});
+
+gulp.task('default', ['watch', 'convertCss', 'html', 'connect']);
+
+gulp.task('watch', function() {
+	gulp.watch('scss/styles.scss', ['convertCss']);
+	gulp.watch('index.php', ['html']);
+});
