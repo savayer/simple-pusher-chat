daadfadfcaf11a17301c76e9aa3087981a809bb8
savayer <sava0094@gmail.com>
2018-10-22 21:45:02 +0200

    edited styles

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,7 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect');
 
 gulp.task('connect', function() {
-	connect.server({
-		root: 'dist',
+	connect.server({	
 		livereload: true
 	})
 })
